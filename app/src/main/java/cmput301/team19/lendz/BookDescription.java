0079396d376c83825ee93ce53c43133c4af50411
Kyle McLean <kylem590@gmail.com>
2020-10-25 02:31:11 -0600

    Add constructor to BookDescription to create from a Map's data

--- app/src/main/java/cmput301/team19/lendz/BookDescription.java
+++ app/src/main/java/cmput301/team19/lendz/BookDescription.java
@@ -1,6 +1,16 @@
 package cmput301.team19.lendz;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+
+import java.util.Map;
+
 public class BookDescription {
+    private static final String ISBN_KEY = "isbn";
+    private static final String TITLE_KEY = "title";
+    private static final String AUTHOR_KEY = "author";
+    private static final String DESCRIPTION_KEY = "description";
 
     private String isbn;
     private String title;
@@ -17,6 +27,16 @@ public class BookDescription {
         this.title = title;
         this.author = author;
         this.description = description;
+    }
+
+    /**
+     * Create a BookDescription from a data Map.
+     */
+    public BookDescription(@NonNull Map<String, Object> data) {
+        isbn = (String) data.get(ISBN_KEY);
+        title = (String) data.get(TITLE_KEY);
+        author = (String) data.get(AUTHOR_KEY);
+        description = (String) data.get(DESCRIPTION_KEY);
     }
 
     public String getIsbn() {
