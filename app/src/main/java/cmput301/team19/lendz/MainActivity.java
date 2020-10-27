71a3e19b041c4ad23bd9abd84482017f2001f64a
blxck <anyinamb@ualberta.ca>
2020-10-26 20:36:57 -0600

    Created the tabs, as well as their corresponding fragments

--- app/src/main/java/cmput301/team19/lendz/MainActivity.java
+++ app/src/main/java/cmput301/team19/lendz/MainActivity.java
@@ -1,14 +1,48 @@
 package cmput301.team19.lendz;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.auth.User;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(tabSelected);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new MyBooksFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener tabSelected =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment fragment = null;
+                    int itemID = item.getItemId();
+                    if (itemID == R.id.borrow) {
+                        fragment = new BorrowBookFragment();
+                    }else if (itemID == R.id.my_books) {
+                        fragment = new MyBooksFragment();
+                    }else if (itemID == R.id.notifications) {
+                        fragment = new NotificaitionsFragment();
+                    } else if(itemID == R.id.profile) {
+                        fragment = new NotificaitionsFragment();
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+                    return true;
+                }
+            };
 }
