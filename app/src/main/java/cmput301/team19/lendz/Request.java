9adb2748ef6ebec8475c1c9546ea47e5da2c9fba
Kyle McLean <kylem590@gmail.com>
2020-11-05 02:29:01 -0700

    Improvements to Request class

--- app/src/main/java/cmput301/team19/lendz/Request.java
+++ app/src/main/java/cmput301/team19/lendz/Request.java
@@ -1,13 +1,89 @@
 package cmput301.team19.lendz;
 
-public class Request {
+import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request {
+    private static final String BOOK_KEY = "book";
+    private static final String REQUESTER_KEY = "requester";
+    private static final String STATUS_KEY = "status";
+
+    // Maps request ID to Request object, guaranteeing at most
+    // one Request object for each request.
+    public static HashMap<String, Request> requests;
+
+    /**
+     * Get or create the unique Request object with the given request ID.
+     */
+    public static Request getOrCreate(@NonNull String id) {
+        Request request = requests.get(id);
+        if (request == null) {
+            request = new Request(id);
+            requests.put(id, request);
+        }
+        return request;
+    }
+
+    private final String id;
+    private Book book;
     private User requester;
     private RequestStatus status;
 
-    public Request(User requester, RequestStatus status) {
-        this.requester = requester;
-        this.status = status;
+    private boolean loaded;
+
+    private Request(@NonNull String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the associated DocumentReference of this Request
+     */
+    public DocumentReference getDocumentReference() {
+        return FirebaseFirestore.getInstance().collection("requests").document(id);
+    }
+
+    /**
+     * Updates this Request object with data from a Firebase DocumentSnapshot.
+     * @param doc DocumentSnapshot to load from
+     */
+    public void load(@NonNull DocumentSnapshot doc) {
+        loaded = true;
+
+        book = Book.getOrCreate(doc.getDocumentReference(BOOK_KEY).getId());
+        requester = User.getOrCreate(doc.getDocumentReference(REQUESTER_KEY).getId());
+        Long statusLong = doc.getLong(STATUS_KEY);
+        if (statusLong == null) {
+            status = RequestStatus.SENT;
+        } else {
+            status = RequestStatus.values()[statusLong.intValue()];
+        }
+    }
+
+    /**
+     * Converts this Request object to a Map.
+     */
+    public Map<String, Object> toData() {
+        Map<String, Object> map = new HashMap<>();
+        map.put(BOOK_KEY, Book.documentOf(book.getId()));
+        map.put(REQUESTER_KEY, User.documentOf(requester.getId()));
+        map.put(STATUS_KEY, status.ordinal());
+        return map;
+    }
+
+    /**
+     * Store the current state of this Request to the Firestore database.
+     * @return Task of the store
+     */
+    public Task<Void> store() {
+        return getDocumentReference().set(toData(), SetOptions.merge());
     }
 
     public User getRequester() {
@@ -19,4 +95,8 @@ public class Request {
     }
 
     public RequestStatus getStatus() { return status; }
+
+    public boolean isLoaded() {
+        return loaded;
+    }
 }
