2f45757e76e4b7a7d7e91b56ff8ef78e7d85677c
Andrews Essilfie <essilfie@ualberta.ca>
2020-11-06 02:26:20 -0700

    testing for EditBookTest

--- app/src/androidTest/java/cmput301/team19/lendz/EditBookTest.java
+++ app/src/androidTest/java/cmput301/team19/lendz/EditBookTest.java
@@ -118,29 +118,7 @@ public class EditBookTest {
 
     }
 
-    public class MyViewAction {
 
-        public static ViewAction clickChildViewWithId(final int id) {
-            return new ViewAction() {
-                @Override
-                public Matcher<View> getConstraints() {
-                    return null;
-                }
-
-                @Override
-                public String getDescription() {
-                    return "Click on a child view with specified id.";
-                }
-
-                @Override
-                public void perform(UiController uiController, View view) {
-                    View v = view.findViewById(id);
-                    v.performClick();
-                }
-            };
-        }
-
-    }
 
 
 }
