8671c2d93369ee28ba7cdb4bb5b60c0c476c3c04
Isaac Mensah <imensah@ualberta.ca>
2020-11-10 15:11:05 +0000

    Merge remote-tracking branch 'origin/viewEditBook' into Isaac
                                                         # Conflicts:
                                                         #	app/src/main/AndroidManifest.xml
                                                         #	app/src/main/java/cmput301/team19/lendz/Book.java

--- app/src/main/java/cmput301/team19/lendz/ViewRequestActivity.java
+++ app/src/main/java/cmput301/team19/lendz/ViewRequestActivity.java
@@ -1,7 +1,11 @@
 package cmput301.team19.lendz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,7 +21,7 @@ public class ViewRequestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_book_request);
         requestBookListView = findViewById(R.id.requestListView);
 
         BorrowerInfo newOne = new BorrowerInfo("Andrews", "https://firebasestorage.googleapis.com/v0/b/lendz-7eb71.appspot.com/o/BookImages%2FQQc5i9NnqjGSeT4Wxq41?alt=media&token=6b795d0d-d809-45e6-87e2-8a2502ceb664",
@@ -33,6 +37,16 @@ public class ViewRequestActivity extends AppCompatActivity {
         //connect ListView to its array content using a custom adapter
         adapter = new ViewRequestCustomAdapter(this, R.layout.view_book_request, BorrowerInfoArray.getInstance().getArray());
         requestBookListView.setAdapter(adapter);
+
+
+//        requestBookListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+//
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+////                Toast.makeText(ViewRequestActivity.this,"woow",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
 
     }
 }
--- app/src/main/java/cmput301/team19/lendz/ViewRequestCustomAdapter.java
+++ app/src/main/java/cmput301/team19/lendz/ViewRequestCustomAdapter.java
@@ -1,14 +1,22 @@
 package cmput301.team19.lendz;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -17,30 +25,75 @@ public class ViewRequestCustomAdapter extends ArrayAdapter<BorrowerInfo> {
     private Context mContext;
     int mResource;
 
+
     public ViewRequestCustomAdapter(@NonNull Context context, int resource, @NonNull List<BorrowerInfo> objects) {
         super(context, resource, objects);
         mContext = context;
         mResource = resource;
     }
 
+    static class ViewHolder {
+        TextView full_name;
+        TextView time_stamp;
+        ImageView owner_image;
+        ImageButton accept ;
+        ImageButton decline;
+        int position;
+    }
+
+    /**
+     * custom array adapter getview.
+     * makes only accept request btn and decline btn clickable
+     */
     @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View v = convertView;
+        ViewHolder viewHolder;
+
+        if (v == null) {
+            LayoutInflater layoutInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v =  layoutInflater.inflate(R.layout.view_book_request,null);
+            viewHolder = new ViewHolder();
+            viewHolder.full_name = (TextView) v.findViewById(R.id.n1);
+            viewHolder.time_stamp = (TextView) v.findViewById(R.id.n2);
+            viewHolder.owner_image = (ImageView) v.findViewById(R.id.requestImage);
+            viewHolder.position = position;
+            viewHolder.accept = (ImageButton) v.findViewById(R.id.acceptRequest);
+            viewHolder.decline = (ImageButton) v.findViewById(R.id.declineRequest);
+            v.setTag(viewHolder);
+        }else{
+            viewHolder = (ViewHolder) v.getTag();
+        }
         String fullName = getItem(position).getFullName();
-       // String photo = getItem(position).getPhoto();
+        String photo = getItem(position).getPhoto();
         String timeStamp = getItem(position).getTimeStamp();
 
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        convertView = inflater.inflate(mResource,parent,false);
-        TextView full_name = (TextView) convertView.findViewById(R.id.textView8);
-        TextView time_stamp = (TextView) convertView.findViewById(R.id.textView9);
-        //ImageView owner_image = (ImageView) convertView.findViewById(R.id.requestImage);
+        viewHolder.full_name.setText(fullName);
+        viewHolder.time_stamp.setText(timeStamp);
+        Picasso.get().load(photo).into(viewHolder.owner_image);
 
-        full_name.setText(fullName);
-        time_stamp.setText(timeStamp);
-        //Picasso.get().load(photo).into(owner_image);
+        //Handle buttons and add onClickListeners
+        viewHolder.decline.setOnClickListener(new View.OnClickListener(){
 
-        return convertView;
+            @Override
+            public void onClick(View v) {
+                //decline request
+                Toast.makeText(getContext(),"decline btn tapped at "+ String.valueOf(position),Toast.LENGTH_SHORT).show();
+                notifyDataSetChanged();
+            }
+        });
+
+        viewHolder.accept.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //accept request
+                Toast.makeText(getContext(),"accept btn tapped at "+ String.valueOf(position),Toast.LENGTH_SHORT).show();
+                notifyDataSetChanged();
+            }
+        });
+
+        return v;
     };
 
 }
