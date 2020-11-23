e207868b03642c472454d4043104957a3bb4418f
Isaac Mensah <imensah@ualberta.ca>
2020-11-23 01:51:25 +0000

    Merge remote-tracking branch 'origin/viewEditBook' into Isaac
                                                         # Conflicts:
                                                         #	app/src/main/AndroidManifest.xml
                                                         #	app/src/main/java/cmput301/team19/lendz/Book.java

--- app/src/main/java/cmput301/team19/lendz/MapsFragment.java
+++ app/src/main/java/cmput301/team19/lendz/MapsFragment.java
@@ -1,15 +1,14 @@
 package cmput301.team19.lendz;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
