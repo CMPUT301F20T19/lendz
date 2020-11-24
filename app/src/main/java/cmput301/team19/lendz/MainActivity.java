8a49a7d4ea2910cbab051233a1f284b3f117c94a
Kyle McLean <kylem590@gmail.com>
2020-11-23 22:05:28 -0700

    Make MainActivity display action bar back button for navigating back through fragments

--- app/src/main/java/cmput301/team19/lendz/MainActivity.java
+++ app/src/main/java/cmput301/team19/lendz/MainActivity.java
@@ -1,27 +1,19 @@
 package cmput301.team19.lendz;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.MenuItem;
-import android.view.View;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.auth.User;
 
 /**
  * this activity contains the bottom navigation of the app
  */
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "USER_ID" ;
-    private BottomNavigationView bottomNavigationView;
-
-
     private String uid;
 
     @Override
@@ -29,44 +21,55 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        getUserID();
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        if (User.getCurrentUser() != null) {
+            uid = User.getCurrentUser().getId();
+        } else {
+            uid = null;
         }
 
-        bottomNavigationView = findViewById(R.id.bottomNav);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(tabSelected);
         getSupportFragmentManager().beginTransaction().replace(R.id.container, new BorrowBookFragment()).commit();
+
+        getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+            @Override
+            public void onBackStackChanged() {
+                updateBackButton();
+            }
+        });
+        updateBackButton();
     }
 
-    private void getUserID() {
-        Intent intent = getIntent();
-        if(intent.hasExtra("userID")) {
-          uid = (String) intent.getSerializableExtra("userID");
-            Log.d(TAG, "getUserID: " + uid);
-        }
+    private void updateBackButton() {
+        boolean enabled = getSupportFragmentManager().getBackStackEntryCount() > 0;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(enabled);
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        getSupportFragmentManager().popBackStack();
+        return true;
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener tabSelected =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
                     Fragment fragment = null;
                     int itemID = item.getItemId();
                     if (itemID == R.id.borrow) {
                         fragment = new BorrowBookFragment();
-                    }else if (itemID == R.id.my_books) {
+                    } else if (itemID == R.id.my_books) {
                         fragment = MyBooksFragment.newInstance(uid);
-                    }else if (itemID == R.id.notifications) {
+                    } else if (itemID == R.id.notifications) {
                         fragment = NotificationsFragment.newInstance();
-                    } else if(itemID == R.id.profile) {
+                    } else if (itemID == R.id.profile) {
                         fragment = ViewUserProfileFragment.newInstance(uid);
                     }
-                    if(fragment != null) {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+                    if (fragment != null) {
+                        getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
                     }
                     return true;
                 }
