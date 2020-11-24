6fa511eb2c03602f5d0730ec7a5d8bc7b84d09ed
Kyle McLean <kylem590@gmail.com>
2020-11-23 22:13:33 -0700

    Rename/delete resource files

--- app/src/main/java/cmput301/team19/lendz/SearchViewAdapter.java
+++ app/src/main/java/cmput301/team19/lendz/SearchViewAdapter.java
@@ -30,7 +30,7 @@ public class SearchViewAdapter extends RecyclerView.Adapter<SearchViewAdapter.Vi
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.book_content,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_book_list_item,parent,false);
         return new ViewHolder(view,onBookClickListener);
     }
 
