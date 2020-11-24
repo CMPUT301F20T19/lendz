050449b3ef3b8a2611c73b31d1fbab4c7ce5b4af
Kyle McLean <kylem590@gmail.com>
2020-11-23 22:02:24 -0700

    Update LocationTest to test toData() method

--- app/src/androidTest/java/cmput301/team19/lendz/LocationTest.java
+++ app/src/androidTest/java/cmput301/team19/lendz/LocationTest.java
@@ -4,6 +4,9 @@ import com.google.firebase.firestore.GeoPoint;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 /**
@@ -14,14 +17,18 @@ public class LocationTest {
      * Tests GeoPoint constructor.
      */
     @Test
-    public void testFromGeoPoint() {
+    public void testFromData() {
         double latitude = 21.8;
         double longitude = 103.5;
+        Map<String, Object> data = new HashMap<>();
+        data.put("geopoint", new GeoPoint(latitude, longitude));
+        String address = "placeworld";
+        data.put("address", address);
 
-        GeoPoint geoPoint = new GeoPoint(latitude, longitude);
-        Location location = new Location(geoPoint);
+        Location location = new Location(data);
 
         assertEquals(latitude, location.getLat(), 0);
         assertEquals(longitude, location.getLon(), 0);
+        assertEquals(address, location.getAddress());
     }
 }
