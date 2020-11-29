f8f6ab63f78c55fb27790fe94d22c75cdc2f6805
Kyle McLean <kylem590@gmail.com>
2020-11-29 15:20:43 -0700

    Clean up SignUpActivity

--- app/src/main/java/cmput301/team19/lendz/SignUpActivity.java
+++ app/src/main/java/cmput301/team19/lendz/SignUpActivity.java
@@ -1,12 +1,8 @@
 package cmput301.team19.lendz;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.BoringLayout;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
@@ -17,8 +13,10 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -27,10 +25,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.squareup.picasso.Picasso;
 
 /**
  * This is the class that handles signing up a new user
@@ -44,19 +38,17 @@ public class SignUpActivity extends AppCompatActivity {
     private EditText usernameEditText;
     private EditText fullnameEditText;
     private EditText emailEditText;
-    private EditText passWordEditText;
+    private EditText passwordEditText;
     private EditText phoneNumberEditText;
-    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        setImageView();
         hidePassword();
         signUpBtn = findViewById(R.id.signUp_button);
         usernameEditText = findViewById(R.id.editText_signup_username);
-        passWordEditText = findViewById(R.id.editText_signup_password);
+        passwordEditText = findViewById(R.id.editText_signup_password);
         fullnameEditText = findViewById(R.id.editText_signup_full_name);
         phoneNumberEditText = findViewById(R.id.editText_signup_phone_number);
         emailEditText = findViewById(R.id.editText_signup_email);
@@ -65,33 +57,33 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                passWordEditText = findViewById(R.id.editText_signup_password);
+                passwordEditText = findViewById(R.id.editText_signup_password);
                 emailEditText = findViewById(R.id.editText_signup_email);
                 String username = usernameEditText.getText().toString();
                 String fullname = fullnameEditText.getText().toString();
-                String phonenumber = phoneNumberEditText.getText().toString();
+                String phoneNumber = phoneNumberEditText.getText().toString();
                 final FirebaseAuth mFirebaseAuth= FirebaseAuth.getInstance();
                 final String email = emailEditText.getText().toString();
-                String pwd = passWordEditText.getText().toString();
+                String pwd = passwordEditText.getText().toString();
                 if(!isValidEmail(email))
                 {
-                    Toast.makeText(SignUpActivity.this, "Invalid Email", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUpActivity.this, "Invalid email address", Toast.LENGTH_LONG).show();
                 }
-                else if(!(phonenumber.length() == 10))
+                else if(!PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber))
                 {
-                    Toast.makeText(SignUpActivity.this, "Invalid Phone Number", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUpActivity.this, "Invalid phone number", Toast.LENGTH_LONG).show();
                 }
-                else if(!(pwd.length() >= 6))
+                else if(pwd.length() < 6)
                 {
-                    Toast.makeText(SignUpActivity.this, "Password Must be atleast 6 characters ", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUpActivity.this, "Password must be at least 6 characters", Toast.LENGTH_LONG).show();
                 }
-                else if(username.isEmpty())
+                else if(username.trim().isEmpty())
                 {
-                    Toast.makeText(SignUpActivity.this, "Enter UserName", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUpActivity.this, "Enter user name", Toast.LENGTH_LONG).show();
                 }
-                else if(fullname.isEmpty())
+                else if(fullname.trim().isEmpty())
                 {
-                    Toast.makeText(SignUpActivity.this, "Enter Full Name", Toast.LENGTH_LONG).show();
+                    Toast.makeText(SignUpActivity.this, "Enter full Name", Toast.LENGTH_LONG).show();
                 }
                 else {
                     mFirebaseAuth.createUserWithEmailAndPassword(email, pwd)
@@ -106,7 +98,6 @@ public class SignUpActivity extends AppCompatActivity {
                                             }
                                             else {
                                                 String uid = createUser(mFirebaseAuth);
-                                                Toast.makeText(SignUpActivity.this, "SignUp successful", Toast.LENGTH_LONG).show();
 
                                                 //Send Uid to Main Activity
                                                 Bundle bundle = new Bundle();
@@ -152,48 +143,15 @@ public class SignUpActivity extends AppCompatActivity {
      * @param user
      * user to be stored
      */
-    public void storeUser(User user )
-    {
-        user.store()
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(SignUpActivity.this, "User profile Saved ", Toast.LENGTH_LONG).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+    public void storeUser(User user) {
+        user.store().addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(SignUpActivity.this, "Unable to save user profile", Toast.LENGTH_LONG).show();
+                Toast.makeText(SignUpActivity.this, "Unable to save user profile",
+                        Toast.LENGTH_LONG).show();
                 Log.e("err", "err", e);
             }
         });
-    }
-
-
-    /**
-     * sets image view to an image stored in firestore
-     */
-    public void setImageView() {
-        imageView = findViewById(R.id.signup_app_logo);
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        String imageUrl = "gs://lendz-7eb71.appspot.com/appLogo.png";
-        storage.getReferenceFromUrl(imageUrl)
-                .getDownloadUrl()
-                .addOnSuccessListener(new OnSuccessListener<Uri>()
-                      {
-                          @Override
-                          public void onSuccess(Uri uri) {
-                              Picasso.get().load(uri).into(imageView);
-                          }
-                      }
-                )
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e("error","Did not work",e);
-                    }
-                });
-
     }
 
     /**
@@ -207,24 +165,19 @@ public class SignUpActivity extends AppCompatActivity {
         return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
     }
 
-
-
     /**
      * method that handles hiding password
      */
     public void hidePassword(){
-        passWordEditText = findViewById(R.id.editText_signup_password);//Get password editText
-        CheckBox passwordCheckBox = findViewById(R.id.signup_checkbox);//Get hidePassword CheckBox
-        passwordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        passwordEditText = findViewById(R.id.editText_signup_password);//Get password editText
+        CheckBox showPasswordCheckBox = findViewById(R.id.show_password_checkbox);//Get showPassword CheckBox
+        showPasswordCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if(b)//if checked hidePassword
-                {
-                    passWordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
-
-                }
-                else {
-                    passWordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+                if (checked) {
+                    passwordEditText.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    passwordEditText.setTransformationMethod(PasswordTransformationMethod.getInstance());
                 }
             }
         });
