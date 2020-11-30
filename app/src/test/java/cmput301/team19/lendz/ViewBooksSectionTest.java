d87cc10430c6c279b7213ae43cb7048746339668
sohanislam001 <sohanislam001@gmail.com>
2020-11-30 15:13:05 -0700

    Updated UI test for "My Books" tab to match data stored in database.

--- app/src/test/java/cmput301/team19/lendz/ViewBooksSectionTest.java
+++ app/src/test/java/cmput301/team19/lendz/ViewBooksSectionTest.java
@@ -7,24 +7,67 @@ import java.util.ArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+/**
+ * Test ViewBooksSection class.
+ */
 public class ViewBooksSectionTest {
+    /**
+     * Test that getSectionHeader method returns the correct section header text.
+     */
     @Test
     public void testGetSectionHeader() {
-        ViewBooksSection mockSection = new ViewBooksSection("mock section",
+        ViewBooksSection availableBooksSection = new ViewBooksSection("Available Books",
                 new ArrayList<Book>());
-        assertEquals("mock section", mockSection.getSectionHeader());
+        assertEquals("Available Books", availableBooksSection.getSectionHeader());
+
+        ViewBooksSection requestedBooksSection = new ViewBooksSection("Requested Books",
+                new ArrayList<Book>());
+        assertEquals("Requested Books", requestedBooksSection.getSectionHeader());
+
+        ViewBooksSection numericalSection = new ViewBooksSection("123",
+                new ArrayList<Book>());
+        assertEquals("123", numericalSection.getSectionHeader());
+
+        ViewBooksSection alphaNumericalSection = new ViewBooksSection("Section 3",
+                new ArrayList<Book>());
+        assertEquals("Section 3", alphaNumericalSection.getSectionHeader());
+
+        ViewBooksSection spaceSection = new ViewBooksSection(" ",
+                new ArrayList<Book>());
+        assertEquals(" ", spaceSection.getSectionHeader());
+
+        ViewBooksSection emptySection = new ViewBooksSection("",
+                new ArrayList<Book>());
+        assertEquals("", emptySection.getSectionHeader());
     }
 
+    /**
+     * Test that getBooks method returns the correct list of books..
+     */
     @Test
     public void testGetBooks() {
+        // create books for testing
         Book book1 = Book.getOrCreate("123");
         Book book2 = Book.getOrCreate("456");
+        Book book3 = Book.getOrCreate("789");
 
+        // check if getBooks method returns an empty array list
         ArrayList<Book> books = new ArrayList<>();
-        books.add(book1);
-        books.add(book2);
-
         ViewBooksSection mockSection = new ViewBooksSection("mock section",
+                books);
+        assertSame(books, mockSection.getBooks());
+
+
+        // check if getBooks method returns an array list with 1 book
+        books.add(book1);
+        mockSection = new ViewBooksSection("mock section",
+                books);
+        assertSame(books, mockSection.getBooks());
+
+        // check if getBooks method returns an array list with 3 books
+        books.add(book2);
+        books.add(book3);
+        mockSection = new ViewBooksSection("mock section",
                 books);
         assertSame(books, mockSection.getBooks());
     }
